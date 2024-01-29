95a6beb945729db41811ea9475ca7f52a9ab4121
Yash <91522919+YashDevLogs@users.noreply.github.com>
2024-01-29 17:22:45 +0530

    Implementation-of-UI-Selection-Panel-and-Making-Build
    Created a tank selection UI

--- Assets/Scripts/TankSelection.cs
+++ Assets/Scripts/TankSelection.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class TankSelection : MonoBehaviour
 {
-
+    public TankSpawner tankSpawner;
+    public void BlueTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.BlueTank);
+        this.gameObject.SetActive(false);
+    } 
+    public void GreenTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.GreenTank);
+        this.gameObject.SetActive(false);
+    }
+    public void RedTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.RedTank);
+        this.gameObject.SetActive(false);
+    }
 }
